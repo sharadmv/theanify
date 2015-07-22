7f7a0aaf416dc4b249b05dbc707f66a19b87ee30
Sharad Vikram <sharad.vikram@gmail.com>
2015-07-22 01:32:27 -0700

    version update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "theanify",
-    version = "0.0.4",
+    version = "0.0.5",
     author = "Sharad Vikram",
     author_email = "sharad.vikram@gmail.com",
     description = "Allows you to annotate instance methods to be compiled by Theano.",
