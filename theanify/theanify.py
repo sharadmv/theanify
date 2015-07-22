1642ec7245284871b774a65a8bab0d9f9e1be76f
Sharad Vikram <sharad.vikram@gmail.com>
2015-07-22 01:31:32 -0700

    added in logging

--- theanify/theanify.py
+++ theanify/theanify.py
@@ -1,10 +1,10 @@
 import os
+import logging
 import cPickle as pickle
 import sys
 sys.setrecursionlimit(10000)
 
 import theano
-import theano.tensor as T
 
 def theanify(*args, **kwargs):
     def func(f):
@@ -21,6 +21,9 @@ class PreTheano(object):
     def set_instance(self, obj):
         self.obj = obj
 
+    def get_var(self):
+        return self(*self.args)
+
     def __call__(self, *args):
         assert self.obj is not None, "Make sure you call the Theanifiable constructor"
         return self.f(self.obj, *args)
@@ -34,8 +37,10 @@ class Theanifiable(object):
                 obj.set_instance(self)
 
     def compile(self, cache=None):
+        logging.info("Compiling <%s> object..." % self.__class__.__name__)
         cache_location = cache
         attrs = {}
+        attrs['_theanify_vars'] = {}
         if cache and os.path.exists(cache):
             with open(cache, 'rb') as fp:
                 cache = pickle.load(fp)
@@ -45,15 +50,19 @@ class Theanifiable(object):
         for name in dir(self):
             obj = getattr(self, name)
             if isinstance(obj, PreTheano):
+                logging.debug("Compiling <%s> method..." % name)
                 updates = None
                 if obj.updates:
                     updates = getattr(self, obj.updates)(*obj.args)
                 if name in cache:
+                    logging.debug("Using cached copy...")
                     compiled = cache[name]
                 else:
+                    var = obj(*obj.args)
+                    #attrs['_theanify_vars'][name] = var
                     compiled = theano.function(
                         obj.args,
-                        obj(*obj.args),
+                        var,
                         updates=updates
                     )
                     cache[name] = compiled
@@ -71,6 +80,7 @@ class Theanifiable(object):
                 pickle.dump(cache, fp)
         for attr, obj in attrs.items():
             setattr(self, attr, obj)
+        logging.info("Done compiling <%s> object." % self.__class__.__name__)
         return self
 
 class CompiledTheano(object):
