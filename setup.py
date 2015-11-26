1ba234b68f98f94748ce16819fcb324468720de9
Sharad Vikram <sharad.vikram@gmail.com>
2015-11-25 18:35:01 -0800

    explicit dependency

--- setup.py
+++ setup.py
@@ -9,9 +9,9 @@ setup(
     license = "MIT",
     keywords = "theano",
     url = "https://github.com/sharadmv/theanify",
-    install_requires=['theano'],
+    install_requires=['theano==0.7.0'],
     dependency_links = [
-        'git+git://github.com/Theano/Theano.git@0e6e9f5acaa2fd0deb427b3dad7b5a7611e5c8b7'
+        'git+git://github.com/Theano/Theano.git@0e6e9f5acaa2fd0deb427b3dad7b5a7611e5c8b7#egg=Theano-0.7.0'
     ],
     packages=find_packages(include=[
         'theanify'
