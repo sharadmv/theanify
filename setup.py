84274038402c83df9c3de39f904461f61b80b576
Sharad Vikram <sharad.vikram@gmail.com>
2015-11-29 17:18:20 -0800

    added commit

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name = "theanify",
-    version = "0.1.13",
+    version = "0.1.14",
     author = "Sharad Vikram",
     author_email = "sharad.vikram@gmail.com",
     description = "Allows you to annotate instance methods to be compiled by Theano.",
     license = "MIT",
     keywords = "theano",
     url = "https://github.com/sharadmv/theanify",
-    install_requires=['Theano==0.7.0'],
+    install_requires=['Theano==0.8.0beta'],
     dependency_links = [
-        'git+git://github.com/Theano/Theano.git@0e6e9f5acaa2fd0deb427b3dad7b5a7611e5c8b7#egg=Theano-0.7.0'
+        'git+git://github.com/Theano/Theano.git@0e6e9f5acaa2fd0deb427b3dad7b5a7611e5c8b7#egg=Theano-0.8.0beta'
     ],
     packages=[
         'theanify'
